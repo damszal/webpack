b6dfe839a0e7c9b7bf62ec9ab72deea6e0e289bf
damian <damian.szalimow@onet.eu>
2022-05-21 21:54:22 +0200

    add server and html plugin config in json

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,32 @@
-console.log("hello webpack!"),(new class{render(){console.log("dupa");const e=document.createElement("button");e.innerText="dummy button",document.querySelector("body").appendChild(e)}}).render();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("console.log(\"hello webpack!\")\r\n\r\nclass Hello {\r\n    render() {\r\n        console.log(\"dupa\")\r\n        const btn = document.createElement('button');\r\n        btn.innerText = \"dummy button\";\r\n        const body = document.querySelector('body');\r\n        body.appendChild(btn);\r\n    }\r\n}\r\n\r\nconst h = new Hello\r\n\r\nh.render()\n\n//# sourceURL=webpack://start/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;

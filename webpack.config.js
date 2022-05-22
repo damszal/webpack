748c537c3ad610b5d40d73199f9e9d516ffb1833
damian <damian.szalimow@onet.eu>
2022-05-22 02:05:04 +0200

    add css, sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+          test: /\.scss$/i,
+          use:[
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+          ]
       }
     ]
   }

765575db22f8da3e34e677dc6b31e94375192bdd
damian <damian.szalimow@onet.eu>
2022-05-26 02:21:56 +0200

    add common file and config - merge webpack configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,54 +1,23 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common,{
   mode: "development",
-  entry: './src/index.js',
   devServer: {
     static: './dist',
   },
   devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Development',
-      }),
-    // new MiniCssExtractPlugin(
-    //   {
-    //     filename: '[name].css'
-    //   }
-    // )
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
           test: /\.scss$/i,
           use:[
-          //{
-          //   loader: MiniCssExtractPlugin.loader,
-          //   options: {
-          //     //hmr: process.env.NODE_ENV === "development"
-          //   }
-          // },
               {loader: 'css-loader', options: {sourceMap:true}},
               {loader: 'postcss-loader', options: {sourceMap:true}},
               {loader: 'sass-loader', options: {sourceMap:true}}
           ]
       }
     ]
-  }
-};
+  },
+}
+)
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,40 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "production",
-  entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
   devtool: 'inline-source-map',
   plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Development',
-      }),
     new MiniCssExtractPlugin(
       {
         filename: '[name].css'
       }
     )
   ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  // =====>>>>>>> if i add code bellow, the build commend will generate error 
+  // =====>>>>>>> now it has build new file main.js ??? do i need chaknge link in index.html ??
+  // output: {
+  //   filename: 'bundle.js',
+  //   path: path.resolve(__dirname, 'dist'),
+  // },
   module: {
     rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
       {
           test: /\.scss$/i,
           use:[{
